2665078de0dfa63dfb92165aefe81792f3bcfede
Pedro Henrique <pedrowskcartola@gmail.com>
2026-07-21 12:04:58 -0300

    Delete app/guia-teste directory

--- app/guia-teste/page.tsx
+++
@@ -1,8 +0,0 @@
-// Conferência visual do Guia. Rota: /guia-teste.
-// Reescrito do zero para liga: sem extras de mata-mata, sem OneSignal, com Glossário.
-
-import { GuiaScreen } from '@/components/guia'
-
-export default function GuiaTestePage() {
-  return <GuiaScreen />
-}
